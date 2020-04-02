f2bae9845f4d04a84df9d3fa4be059dfb031a313
Leonardo de Oliveira Lourenço <bigodation@gmail.com>
2020-04-02 05:56:17 -0700

    Add basic code structure for stack

---
+++ code/stack/integer-stack/array/autotest.sh
@@ -0,0 +1,40 @@
+#!/bin/bash
+
+readonly PROJECT_ROOT=$( cd "$( dirname "$0" )" && pwd )
+
+readonly C_CPP_EXTENSIONS_REGEX='[.](h|hpp|c|cc|cp|cxx|cpp|CPP|c[+]{2}|C)$'
+
+
+function main()
+{
+  ensure_main_file_exists "src"
+  ensure_main_file_exists "tests"
+
+  while true; do
+  find "$PROJECT_ROOT" -type f |
+  grep -E "$C_CPP_EXTENSIONS_REGEX" |
+  entr -d bash entr_script.sh "$PROJECT_ROOT"
+  done
+}
+
+function ensure_main_file_exists()
+{
+  local main_file=''
+
+  while true; do
+    main_file=$(find "$PROJECT_ROOT/$1" -type f | grep -E "/main$C_CPP_EXTENSIONS_REGEX")
+
+    if [ -z "$main_file" ]
+    then
+      echo -e "\nERROR"
+      echo "No main file found on '$1' folder!"
+      echo "Please name your main file as 'main' with the appropriate extension."
+      echo -e "\nRetrying in 5 seconds..."
+      sleep 5
+    else
+      break
+    fi
+  done
+}
+
+main "$@"
